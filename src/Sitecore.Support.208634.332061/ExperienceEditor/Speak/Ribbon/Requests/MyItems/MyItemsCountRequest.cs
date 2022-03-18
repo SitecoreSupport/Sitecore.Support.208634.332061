02875367f5905db933d873ee04dadf71eb995901
Paul <stupka.paul@gmail.com>
2022-03-18 09:26:40 +0200

    Use the cumulative hotfix for Sitecore 9.3 as a base for the fix.

--- src/Sitecore.Support.208634.332061/ExperienceEditor/Speak/Ribbon/Requests/MyItems/MyItemsCountRequest.cs
+++ src/Sitecore.Support.208634.332061/ExperienceEditor/Speak/Ribbon/Requests/MyItems/MyItemsCountRequest.cs
@@ -1,5 +1,7 @@
-﻿using Sitecore.Configuration;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.Configuration;
 using Sitecore.Data.Locking;
+using Sitecore.DependencyInjection;
 using Sitecore.Diagnostics;
 using Sitecore.ExperienceEditor.Speak.Server.Responses;
 
@@ -7,6 +9,16 @@ namespace Sitecore.Support.ExperienceEditor.Speak.Ribbon.Requests.MyItems
 {
     public class MyItemsCountRequest : Sitecore.ExperienceEditor.Speak.Ribbon.Requests.MyItems.MyItemsCountRequest
     {
+        private readonly UserLockProvider _lockProvider;
+
+        public MyItemsCountRequest() : this(ServiceLocator.ServiceProvider.GetRequiredService<UserLockProvider>())
+        {
+        }
+        public MyItemsCountRequest(UserLockProvider lockProvider)
+        {
+            _lockProvider = lockProvider;
+        }
+
         public override PipelineProcessorResponseValue ProcessRequest()
         {
             Assert.IsNotNull(this.RequestContext.Database, $"Could not get context.Database for requestArgs:{this.Args.Data}");
@@ -23,7 +35,7 @@ namespace Sitecore.Support.ExperienceEditor.Speak.Ribbon.Requests.MyItems
             }
 
             var userLockedItemArgs = new UserLockedItemArgs(Context.User.Identity, database);
-            UserLockProvider.Instance.GetItemsLockedByUser(userLockedItemArgs);
+            _lockProvider.GetItemsLockedByUser(userLockedItemArgs);
             var items = userLockedItemArgs.Result;
             return new PipelineProcessorResponseValue
             {
--- src/Sitecore.Support.208634.332061/SQL Script/ndxItemLock.sql
+++
@@ -1,7 +0,0 @@
-/* Sitecore.Support.208634.332061
-   The script should be executed against the master database
- */
-CREATE NONCLUSTERED INDEX ndxItemLock
-ON [VersionedFields] ([ItemId])
-INCLUDE ([Language], [Version], [Value])
-WHERE FieldId='{001DD393-96C5-490B-924A-B0F25CD9EFD8}'
